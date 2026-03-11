1bb648e2f24d9bbe49aa595668dca0ba98c31b14
肉球 <as2711830@gmail.com>
2026-03-11 14:03:14 +0800

    Day 23: 檔案路由系統，layout.js、page.js、metadata、Link 用法

---
+++ src/app/about/page.js
@@ -0,0 +1,11 @@
+export const metadata = {
+  title: "關於我",
+};
+
+export default function About() {
+  return (
+    <div>
+      <h1>關於我</h1>
+    </div>
+  );
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,6 @@
+export const metadata = {
+  title: "首頁 | My Next App",
+};
 export default function Home() {
   return <h1>首頁</h1>;
 }
